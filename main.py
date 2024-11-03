6f6eb8d42624371588c771a85f9208b8c02f58c9
JuanJosePradaContreras <180313522+JuanJosePradaContreras@users.noreply.github.com>
2024-11-03 17:41:14 -0500

    Excercise 3, exact division

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
-#Ejercicio 2
+#Ejercicio 3
 
-#Cuando la Tierra completa una órbita alrededor del Sol, no han transcurrido exactamente 365 rotaciones sobre sí misma, sino un poco más. 
-# Más precisamente, la diferencia es de más o menos un cuarto de día.
+#Escriba un programa que pida dos números enteros y que calcule la división, indicando si la división es exacta o no.
 
-#Para evitar que las estaciones se desfasen con el calendario, el calendario juliano introdujo la regla de introducir un día adicional en los años divisibles 
-#por 4 (llamados bisiestos), para tomar en consideración los cuatro cuartos de día acumulados.
+#Dividendo: 14
+#Divisor: 5
 
-#Sin embargo, bajo esta regla sigue habiendo un desfase, que es de aproximadamente 3/400 de día.
+#La división no es exacta.
+#Cociente: 2
+#Resto: 4
+#Dividendo: 100
+#Divisor: 10
 
-#Para corregir este desfase, en el año 1582 el papa Gregorio XIII introdujo un nuevo calendario, en el que el último año de cada siglo dejaba de ser bisiesto, 
-# a no ser que fuera divisible por 400.
+#La división es exacta.
+#Cociente: 10
+#Resto: 0
 
-#Escriba un programa que indique si un año es bisiesto o no, teniendo en cuenta cuál era el calendario vigente en ese año:
-
-def es_bisiesto(anio):
-
-    if anio < 1582:
+def division_exacta():
+    while True:
+        try:
+            dividendo = int(input("Introduce the dividend: "))
+            divisor = int(input("Introduce the dividend: "))
+            
+            if divisor == 0:
+                print("The divisor can't be 0.")
+                continue
+            
+            cociente = dividendo // divisor
+            resto = dividendo % divisor
+            
+            if resto == 0:
+                print("The division is exactly.")
+            else:
+                print("The division isn't exactly.")
+            
+            print(f"Cociente: {cociente}")
+            print(f"Resto: {resto}")
+            break
         
-        return anio % 4 == 0
-    else:
-        
-        if anio % 400 == 0:
-            return True
-        
-        elif anio % 100 == 0:
-            return False
-        
-        elif anio % 4 == 0:
-            return True
-        
-        else:
-            return False
-
-anio = int(input("Ingrese un año: "))
-
-if es_bisiesto(anio):
-    print(f"{anio} es bisiesto")
-else:
-    print(f"{anio} no es bisiesto")
+        except ValueError:
+            print("Please just enter integrers.")
