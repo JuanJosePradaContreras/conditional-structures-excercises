ec6b7f6c6ac7eb32b833d781bec40f5f2b976b6d
JuanJosePradaContreras <180313522+JuanJosePradaContreras@users.noreply.github.com>
2024-11-03 19:54:13 -0500

    Ejercicio 8, Age

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
-#Ejercicio 7
+#Ejercicio 8
 
-#Escriba un programa que simule una calculadora básica, este puede realizar operación de suma, resta, multiplicación y división.
-#El programa debe recibir como entrada 2 números reales y un operador, que puede ser +, -, * o /.
-#La salida del programa debe ser el resultado de la operación.
+#Escriba un programa que entregue la edad del usuario a partir de su fecha de nacimiento:
 
-def calculadora():
+#Ingrese su fecha de nacimiento.
+#Dia: 14
+#Mes: 6
+#Anno: 1948
+#Usted tiene 62 annos
+#Por supuesto, el resultado entregado depende del día en que su programa será ejecutado.
 
-    try:
-        num1 = float(input("Introduce el primer número: "))
-        num2 = float(input("Introduce el segundo número: "))
-        operador = input("Introduce el operador (+, -, *, /): ")
+#Para obtener la fecha actual, puede hacerlo usando la función localtime que viene en el módulo time. Los valores se obtienen de la 
+#siguiente manera (suponiendo que hoy es 11 de marzo de 2011):
 
-        if operador == "+":
-            resultado = num1 + num2
-            print(f"El resultado de {num1} + {num2} es: {resultado}")
-        elif operador == "-":
-            resultado = num1 - num2
-            print(f"El resultado de {num1} - {num2} es: {resultado}")
-        elif operador == "*":
-            resultado = num1 * num2
-            print(f"El resultado de {num1} * {num2} es: {resultado}")
-        elif operador == "/":
-            if num2 != 0:
-                resultado = num1 / num2
-                print(f"El resultado de {num1} / {num2} es: {resultado}")
-            else:
-                print("Error: No se puede dividir entre cero.")
-        else:
-            print("Operador no válido. Por favor, utiliza +, -, * o /.")
-    except ValueError:
-        print("Error: Por favor, introduce números válidos.")
+#>>> from time import localtime
+#>>> t = localtime()
+#>>> t.tm_mday
+#11
+#>>> t.tm_mon
+#3
+#>>> t.tm_year
+#2011
+#El programa debe tener en cuenta si el cumpleaños ingresado ya pasó durante este año, o si todavía no ocurre.#
 
-calculadora()
+from time import localtime
+
+def calcular_edad():
+   
+    t = localtime()
+    dia_actual = t.tm_mday
+    mes_actual = t.tm_mon
+    anno_actual = t.tm_year
+    
+    
+    dia_nacimiento = int(input("Ingrese su día de nacimiento: "))
+    mes_nacimiento = int(input("Ingrese su mes de nacimiento: "))
+    anno_nacimiento = int(input("Ingrese su año de nacimiento: "))
+    
+   
+    edad = anno_actual - anno_nacimiento
+    
+    
+    if (mes_nacimiento > mes_actual) or (mes_nacimiento == mes_actual and dia_nacimiento > dia_actual):
+        edad -= 1
+    
+    print(f"Usted tiene {edad} años.")
+
+
+calcular_edad()
